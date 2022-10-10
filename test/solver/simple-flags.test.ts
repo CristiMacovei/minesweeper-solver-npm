5ca0590383ad03299d0dbd855bb77035f51875d7
CristiMacovei <cristimacovei26@gmail.com>
2022-10-10 12:47:12 +0300

    [feat] simple flags rule working

--- test/solver/simple-flags.test.ts
+++ test/solver/simple-flags.test.ts
@@ -145,4 +145,69 @@ describe('Simple flags rule', () => {
       reason: 'simple flag from (1, 1)'
     });
   });
+
+  test('should return empty array for a 2 in a corner', () => {
+    const solver = Minesolver.blank(3,3);
+    solver.setTile(0,0, SolverTileType.OPEN_2);
+
+    const moves = solver.listSimpleFlags();
+    expect(moves.length).toBe(0);
+  });
+
+  test('should return empty array for an unknown cell in a corner', () => {
+    const solver = Minesolver.blank(3,3);
+    solver.setTile(0,2, SolverTileType.OPEN_UNKNOWN);
+
+    const moves = solver.listSimpleFlags();
+    expect(moves.length).toBe(0);
+  });
+
+  test('should place a flag on a 1 with only one empty cell', () => {
+    const solver = Minesolver.blank(4,4);
+    
+    /*
+    0 0  0 -2
+    0 1  1 -2
+    0 1 -2 -2
+    */
+    solver.setTile(0,0, SolverTileType.OPEN_0);
+    solver.setTile(0,1, SolverTileType.OPEN_0);
+    solver.setTile(0,2, SolverTileType.OPEN_0);
+    solver.setTile(1,0, SolverTileType.OPEN_0);
+    solver.setTile(1,1, SolverTileType.OPEN_1);
+    solver.setTile(1,2, SolverTileType.OPEN_1);
+    solver.setTile(2,0, SolverTileType.OPEN_0);
+    solver.setTile(2,1, SolverTileType.OPEN_1);
+
+    const moves = solver.listSimpleFlags();
+    expect(moves.length).toBe(1);
+
+    expect(moves).toContainEqual({
+      type: 'flag',
+      row: 2,
+      col: 2,
+      reason: 'simple flag from (1, 1)'
+    })
+  });
+
+  test.only('shouldn\'t place a flag on a 1 already having a flag', () => {
+    const solver = Minesolver.blank(4,4);
+    
+    /*
+    -1 1  0 -2
+     1 1  1 -2
+     0 1 -2 -2
+    */
+    solver.setTile(0,0, SolverTileType.FLAG);
+    solver.setTile(0,1, SolverTileType.OPEN_1);
+    solver.setTile(0,2, SolverTileType.OPEN_0);
+    solver.setTile(1,0, SolverTileType.OPEN_1);
+    solver.setTile(1,1, SolverTileType.OPEN_1);
+    solver.setTile(1,2, SolverTileType.OPEN_1);
+    solver.setTile(2,0, SolverTileType.OPEN_0);
+    solver.setTile(2,1, SolverTileType.OPEN_1);
+
+    const moves = solver.listSimpleFlags();
+    expect(moves.length).toBe(0);
+  });
 });
